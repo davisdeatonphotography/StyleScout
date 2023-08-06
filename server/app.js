b0f81e9dcc27e4cf2ab6b51910e71c8b21336472
davisdeatonphotography <130251632+davisdeatonphotography@users.noreply.github.com>
2023-08-05 22:13:37 -0500

    Version 0.0.3

--- server/app.js
+++ server/app.js
@@ -118,7 +118,11 @@ function filterCssContent(cssContent) {
 }
 
 async function getCssContentFromUrl(url) {
-  const browser = await puppeteer.launch({ headless: 'new' });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: 'networkidle2' });
 
